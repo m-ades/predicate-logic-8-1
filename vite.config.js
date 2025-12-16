1bf4664bbc1ffa6bb35b9cb097b86398296ebcc3
m-ades <m-ades@users.noreply.github.com>
2025-12-15 22:05:36 -0500

    fix github pages deployment and clean up code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/predicate-logic-8-1/',
+  base: process.env.GITHUB_PAGES_BASE || '/predicate-logic-8-1/',
 })
